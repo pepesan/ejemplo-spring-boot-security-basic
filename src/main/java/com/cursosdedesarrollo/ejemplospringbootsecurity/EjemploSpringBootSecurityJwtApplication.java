0de543dcbc13f77e1573a660cdd5e83f08c562cf
pepesan <pepesan@gmail.com>
2024-09-28 11:18:50 +0200

    Initial commit

---
+++ src/main/java/com/cursosdedesarrollo/ejemplospringbootsecurity/EjemploSpringBootSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.cursosdedesarrollo.ejemplospringbootsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploSpringBootSecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjemploSpringBootSecurityJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cursosdedesarrollo/ejemplospringbootsecurity/EjemploSpringBootSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursosdedesarrollo.ejemplospringbootsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploSpringBootSecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
